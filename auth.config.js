dc7388b2e755858a44cf30a1a869e0c3bcdd799c
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2024-06-26 16:10:40 -0500

    app: migrate v1 app

---
+++ auth.config.js
@@ -0,0 +1,20 @@
+import Google from '@auth/core/providers/google';
+import { defineConfig } from 'auth-astro';
+
+export default defineConfig({
+  providers: [
+    Google({
+      clientId: import.meta.env.GOOGLE_CLIENT_ID,
+      clientSecret: import.meta.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    session: ({ session, token }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: token.sub,
+      },
+    }),
+  }
+});
